eaaa5e3ce885913aa1faf796fcb5fb89dc9178fc
losertnaw1 <45971780+losertnaw1@users.noreply.github.com>
2022-03-24 17:06:46 +0700

    gen_server on nodes

---
+++ helloworld/src/ct_ct.erl
@@ -0,0 +1,140 @@
+%% @author datle
+%% @doc @todo Add description to chat_client.
+
+
+-module(ct_sv).
+-behaviour(gen_server).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([s/1,rs/2,sd/3,get/3]).
+
+s(NodeName) ->
+	gen_server:start_link({local,?MODULE}, ?MODULE, [NodeName], []).
+
+rs(NodeName,Pid) ->
+	io:format("Client ~p registed : ~p ~n", [NodeName,Pid]),
+	chat_server:rs(NodeName,Pid).
+
+
+sd(NodeName, Msg,To) ->
+	gen_server:call(NodeName, {send,Msg,To}).
+
+get(NodeName,_To, Msg) ->
+	gen_server:call({get,Msg}, NodeName).
+%% ====================================================================
+%% Behavioural functions
+%% ====================================================================
+
+
+%% init/1
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:init-1">gen_server:init/1</a>
+-spec init(Args :: term()) -> Result when
+	Result :: {ok, State}
+			| {ok, State, Timeout}
+			| {ok, State, hibernate}
+			| {stop, Reason :: term()}
+			| ignore,
+	State :: term(),
+	Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+init(NodeName) ->
+    {ok, {NodeName}}.
+
+
+%% handle_call/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:handle_call-3">gen_server:handle_call/3</a>
+-spec handle_call(Request :: term(), From :: {pid(), Tag :: term()}, State :: term()) -> Result when
+	Result :: {reply, Reply, NewState}
+			| {reply, Reply, NewState, Timeout}
+			| {reply, Reply, NewState, hibernate}
+			| {noreply, NewState}
+			| {noreply, NewState, Timeout}
+			| {noreply, NewState, hibernate}
+			| {stop, Reason, Reply, NewState}
+			| {stop, Reason, NewState},
+	Reply :: term(),
+	NewState :: term(),
+	Timeout :: non_neg_integer() | infinity,
+	Reason :: term().
+%% ====================================================================
+handle_call({send,Msg,To}, From, State) ->
+	case maps:find(To, State) of
+		{ok, online} -> chat_server:sd(From,To,Msg);
+		error -> io:format("Client offline")
+	end;
+handle_call({get,Msg}, From, State) ->
+	case lists:member(From, State) of
+		true -> io:format("Sent_");
+		false -> io:format("Got message from ~p : ~p ~n",[From,Msg])
+	end;
+handle_call(_Request, _From, State) ->
+    io:format("State : ~p~n",[State]),
+    {reply, {ok,handleInfo,client}, State}.
+
+
+%% handle_cast/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:handle_cast-2">gen_server:handle_cast/2</a>
+-spec handle_cast(Request :: term(), State :: term()) -> Result when
+	Result :: {noreply, NewState}
+			| {noreply, NewState, Timeout}
+			| {noreply, NewState, hibernate}
+			| {stop, Reason :: term(), NewState},
+	NewState :: term(),
+	Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+handle_cast(Msg, State) ->
+	io:format("handleCast client : ~p , ~p ~n", [State,Msg] ),
+    {noreply, State}.
+
+
+%% handle_info/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:handle_info-2">gen_server:handle_info/2</a>
+-spec handle_info(Info :: timeout | term(), State :: term()) -> Result when
+	Result :: {noreply, NewState}
+			| {noreply, NewState, Timeout}
+			| {noreply, NewState, hibernate}
+			| {stop, Reason :: term(), NewState},
+	NewState :: term(),
+	Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+handle_info(_Info, State) ->
+	io:format("handleInfo client : ~p ~n", [State] ),
+    {noreply, State}.
+
+
+%% terminate/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:terminate-2">gen_server:terminate/2</a>
+-spec terminate(Reason, State :: term()) -> Any :: term() when
+	Reason :: normal
+			| shutdown
+			| {shutdown, term()}
+			| term().
+%% ====================================================================
+terminate(_Reason, _State) ->
+    ok.
+
+
+%% code_change/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:code_change-3">gen_server:code_change/3</a>
+-spec code_change(OldVsn, State :: term(), Extra :: term()) -> Result when
+	Result :: {ok, NewState :: term()} | {error, Reason :: term()},
+	OldVsn :: Vsn | {down, Vsn},
+	Vsn :: term().
+%% ====================================================================
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
